3493c941eeeff1e831bab1e0a755976d113b77a2
Adanrleon <113628860+Adanrleon@users.noreply.github.com>
2022-11-16 23:09:53 -0400

    Add files via upload

---
+++ Mono.js
@@ -0,0 +1,13 @@
+let ASmile = true;
+let BSmile = true;
+
+let Mensaje = "Los monos estan en grandes problemas"
+
+if (ASmile == false && BSmile == false || ASmile == true && BSmile == true)
+{
+    console.log(Mensaje)
+}
+else 
+{
+    console.log("Los monos no estan en grandes problemas")
+}
